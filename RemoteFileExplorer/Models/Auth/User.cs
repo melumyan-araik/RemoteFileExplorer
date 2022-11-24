2db9dc61e8e38053cd169f4c7e8fbe12de13232d
Araik <iaraik99@gmail.com>
2022-11-24 14:37:16 +0900

    Revert "Revert "Поправил модели""
    This reverts commit 21edc60fa7e27f667aa7ded684a9d9816aabb1e1.

--- RemoteFileExplorer/Models/Auth/User.cs
+++ RemoteFileExplorer/Models/Auth/User.cs
@@ -1,18 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RemoteFileExplorer.Models
+﻿namespace RemoteFileExplorer.Models.Auth
 {
     public class User
     {
         [Key]
         public Guid Id { get; set; }
         [Required]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         [Required]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
         public double DiskSpace { get; set; } = Math.Pow(1024, 3) * 10;
         public double UsedSpace { get; set; } = 0;
         public string? Avatar { get; set; }
-        public List<File>? Files { get; set; }
     }
 }
